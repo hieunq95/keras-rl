6e10b03fc083914bb7c4eb9e280e656f9d81221f
Hieu Nguyen <hieu1204@gmail.com>
2019-08-13 21:56:19 +0800

    update q-learning

--- mcml-blockchain/q-learning.py
+++ mcml-blockchain/q-learning.py
@@ -6,7 +6,7 @@ import gym
 import matplotlib.pyplot as plt
 import xlsxwriter
 import random
-from environment import Environment
+from environment import Environment, MyProcessor
 from writer_v1 import MCMLWriter
 
 mempool = []
@@ -15,6 +15,7 @@ writer = MCMLWriter(workbook)
 
 env = Environment(mempool, writer)
 env.reset()
+processor = MyProcessor()
 print(env.observation_space, env.action_space)
 
 # Hyperparameters
@@ -22,7 +23,7 @@ alpha = 0.1
 gamma = 0.9
 epsilon = 0.1
 # Initialize q-table
-state_space_size = 2560  # 4 x 4 x 4 x 4 x 10
+state_space_size = 1024  # 4 x 4 x 4 x 4 x 4
 action_space_size = 1024  # 4 x 4 x 4 x 4 x 4
 q_table = np.zeros((state_space_size, action_space_size))
 print(q_table.shape)
@@ -42,19 +43,20 @@ for i in range(NB_EPISODES):
             action = env.action_space.sample()
         else:
             action = np.argmax(q_table[state])
-
+        action = processor.process_action(action)
         next_state, reward, done, info = env.step(action)
-
+        print(next_state, reward, done, info)
         old_value = q_table[state, action]
         next_max = np.max(q_table[next_state])
 
         new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
-        q_table[state,action] = new_value
+        q_table[state, action] = new_value
 
         state = next_state
 
         episode_reward += reward
 
-        print('Episode: {}, Ep_reward: {},'.format(episode, reward))
+    episode += 1
+    print('Episode: {}, Ep_reward: {},'.format(episode, reward))
 
 
